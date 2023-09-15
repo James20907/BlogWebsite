86c52f6694e61f1cd1e7ada494bf79bffb418e3f
james <james20907@yahoo.com.tw>
2023-09-15 17:50:40 +0800

    ver.0.001

---
+++ BlogWebsite/Controllers/MessageBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebsite.Controllers
+{
+    public class MessageBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
